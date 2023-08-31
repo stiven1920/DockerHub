56368aeeacf0f6cb5859fe3ea7e7775c01aadea6
stiven1920 <jhersson.jojoa.6265@eam.edu.co>
2023-08-31 02:31:02 +0000

    taller 2

---
+++ taller2/script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+variable=${1}
+
+mensaje(){
+echo "cargando ...."
+echo "Accediendo a las peliculas"
+echo "Obteniendo información bancaria"
+echo "su sistema a sido vulnerado...."
+echo ""
+echo "obteniendo la fecha"
+}
+
+echo ""
+fecha(){
+date
+echo ""
+echo "casi no lo logro: " ${variable}
+}
+
+#Documentacion
+#leer el archivo de los usuarios
+
+echo ""
+
+copiarArchivo(){
+echo "vamos hasta la carpeta"
+echo ""
+echo "cd /var/www/html/"
+echo "damos ls para revisar que carpetas"
+echo "copiamos el archvo html"
+echo ""
+echo "cp (archivo) /home/stiven1920/EAM_ECS/taller2"
+echo "lista los archivos ls"
+}
+
+fecha
+mensaje
+copiarArchivo
